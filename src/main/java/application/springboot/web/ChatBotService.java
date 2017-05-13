b61ae8d4855cc4e58632b09a02fc40ebe146522b
rasocham <rasocham@students.zcu.cz>
2017-05-13 12:52:55 +0200

    data

---
+++ src/main/java/application/springboot/web/ChatBotService.java
@@ -0,0 +1,8 @@
+package application.springboot.web;
+
+/**
+ * @author Marek Rasocha
+ *         date 02.05.2017.
+ */
+public interface ChatBotService {
+}
---
+++ src/main/java/application/springboot/web/ChatBotServiceImpl.java
@@ -0,0 +1,39 @@
+package application.springboot.web;
+
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+
+/**
+ * @author Marek Rasocha
+ *         date 02.05.2017.
+ */
+public class ChatBotServiceImpl {
+    private ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2017_02_03,"b4d5e338-b5d2-4b91-bc50-67a4f73e2349", "om4MLqXIVcj0");
+
+
+    public ChatBotServiceImpl() {
+    }
+    // TODO: upravit
+    public String sendMessage(String messageContent) {
+        ConversationService service = new ConversationService("2017-04-21");
+        service.setUsernameAndPassword("{username}", "{password}");
+
+
+        MessageRequest newMessage = new MessageRequest.Builder()
+                .inputText(messageContent)
+                        // Replace with the context obtained from the initial request
+                        //.context(...)
+                .build();
+
+        String workspaceId = "25dfa8a0-0263-471b-8980-317e68c30488";
+
+        MessageResponse response = service
+                .message(workspaceId, newMessage)
+                .execute();
+
+        System.out.println(response);
+        return null;
+    }
+}
