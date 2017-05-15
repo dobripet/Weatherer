8d42e55e043447f0f15b268f882d260c1fafc335
rasocham <rasocham@students.zcu.cz>
2017-05-15 20:42:57 +0200

    Data finder and dataSorter

--- src/main/java/cz/zcu/kiv/si/sportbot/service/DataFinder.java
+++ src/main/java/cz/zcu/kiv/si/sportbot/service/DataFinder.java
@@ -1,7 +1,9 @@
 package cz.zcu.kiv.si.sportbot.service;
 
+import cz.zcu.kiv.si.sportbot.dataLoader.enums.Day;
 import cz.zcu.kiv.si.sportbot.dataLoader.enums.SportGroup;
 import cz.zcu.kiv.si.sportbot.dataLoader.enums.SportType;
+import cz.zcu.kiv.si.sportbot.dataLoader.object.OpeningTime;
 import cz.zcu.kiv.si.sportbot.dataLoader.object.SportPlace;
 
 import java.util.List;
@@ -19,18 +21,26 @@ public interface DataFinder {
     List<SportPlace> findSport(SportType sportType);
 
     /**
+     * vyhleda vsechny sporty, ktere jsou typu ve sporttypes
+     * @param sportTypes sportTypes
+     * @return list odpovidajicich instituci
+     */
+    List<SportPlace> findSport(List<SportType> sportTypes);
+
+    /**
      * najde sportplace podle typu sportu, kde sport jde delat venku
      * @param sportType sportType
      * @return list nalazenych sportplace
      */
     List<SportPlace> findOutsideSport(SportType sportType);
+    List<SportPlace> findOutsideSport();
     /**
      * najde sportplace podle typu sportu, kde sport jde delat vevnitr
      * @param sportType sportType
      * @return list nalazenych sportplace
      */
     List<SportPlace> findInsideSport(SportType sportType);
-
+    List<SportPlace> findInsideSport();
     /**
      * nejde sporty ze skupiny
      * @param sportGroup sport type
@@ -43,4 +53,6 @@ public interface DataFinder {
      * @return list nalazenych sportplace
      */
     List<SportPlace> findSportFree();
+
+
 }
